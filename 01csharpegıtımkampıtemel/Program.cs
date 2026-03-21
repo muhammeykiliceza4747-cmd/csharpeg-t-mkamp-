33ed6e9d24314f89c07c65de753d2284a60bf55b
muham <muham@MUHAMMED>
2026-03-22 01:03:16 +0300

    Add project files.

---
+++ 01csharpegıtımkampıtemel/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01csharpegıtımkampıtemel
+{
+    internal class Program
+    {
+        private static object menu;
+
+        static void Main(string[] args)
+        {
+            #region yazdırma
+
+            ////{
+            ////    //Console.Write("merhaba dunya my ");
+            ////    Console.WriteLine("merhaba dunya my ");
+            ////    Console.WriteLine("merhaba dunya my "); 
+            ///
+            #endregion
+            #region değişkenler
+            //string name;
+            //name = "muhammmmed";
+            //Console.WriteLine(name);
+            //string costumername;
+            //string costumerlastname;
+            //string costumerphone;
+
+            //costumername = "muhammed";
+            //costumerlastname = "kılıç";
+            //costumerphone = "05555555555"; 
+            //Console.WriteLine('Musteri adı' + costumername + ' ' + (costumerlastname);
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ıkıncı drs/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ıkıncı_drs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region variables 
+            //double number;
+            //number = 12.5; 
+            //Console.WriteLine(number); 
+            //Console.WriteLine("****** fıyat lıstsı ******");
+            //double appleprice, bananaeprice, orangeprice;
+
+            //appleprice = 12.5;
+            //bananaeprice = 1.1;
+            //orangeprice = 3.3;
+            //Console.WriteLine(" kg portokal fiyatı: " + appleprice + "tl");
+            //Console.WriteLine(" kg elma fiyatı: " + orangeprice + "tl");
+            //Console.WriteLine(" kg muz fiyatı: " + bananaeprice + "tl");
+            //double aplegram, bananagram, orangegram;
+            //aplegram = 1.233; bananagram = 3.337; orangegram = 747;
+
+            //double apletotalprice = aplegram * appleprice;
+            //double orangetotalpric = bananagram * bananaeprice;
+            //double bananatotalprice = orangegram * orangeprice;
+            //Console.WriteLine("alınan urun: elma -" + "bırım fiyat:" + appleprice +  " - gramaj: "+ aplegram +" toplam tutar " + apletotalprice );
+            //Console.WriteLine("alınan urun: muz -" + "bırım fiyat:" + bananaeprice +  " - gramaj: "+ bananagram +" toplam tutar " + bananatotalprice );
+            //Console.WriteLine("alınan urun: portakal -" + "bırım fiyat:" + orangeprice +  " - gramaj: "+ orangegram +" toplam tutar " + orangetotalpric );
+            //double totalprice = apletotalprice + orangetotalpric + bananatotalprice;
+            //Console.WriteLine("toplam tutar: " + totalprice + "tl");
+            #endregion
+            #region char 
+            char symbol;
+            symbol = 'a';
+            Console.WriteLine(symbol);
+            #endregion
+            #region klavterden veri alma 
+            //Console.WriteLine("kılıc hava yolları yolcu bilgi sistemi");
+            //string passengernam , passemgerlastname ,passengernumbber , passngerage;
+            //Console.Write("yolcu adı: " );
+
+            //passengernam =Console.ReadLine();
+
+            //Console.Write("yolcu soyadı: ");    
+
+
+            //passemgerlastname =Console.ReadLine();   
+
+            //    Console.WriteLine("YOLCU" + passengernam + "" + passemgerlastname );
+
+            #endregion
+            #region klavye donus alma 
+            // int number1, number2, number3;
+            // number1 =12; number2 = 5; number3 = 7;  
+            //int number1price, number2prıce, number3prıce;
+            // Console.WriteLine("kac tane aldın yarrağım");
+            // number1price = int.Parse(Console.ReadLine());
+
+
+            // Console.WriteLine("kac tane aldın yarrağım");
+            // number2prıce = int.Parse(Console.ReadLine());
+
+
+            // Console.WriteLine("kac tane aldın yarrağım");
+            // number3prıce = int.Parse(Console.ReadLine());
+
+            // int totalprice = number1 * number1price + number2 * number2prıce + number3 * number3prıce;
+
+            // Console.WriteLine("toplam tutar: " + totalprice + "tl");
+
+            #endregion
+
+            #region klavye donus alma 3
+
+            //double number1, number2, number3;
+
+            //Console.Write("1.sınav");
+
+            //number1 = double.Parse(Console.ReadLine());
+            //Console.Write("2.sınav");
+
+            //number2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("3.sınav");
+
+            //number3 = double.Parse(Console.ReadLine());
+
+            //double result = (number1 + number2 + number3) / 3;
+            //Console.WriteLine();
+            //Console.WriteLine("ortalama: " + result);
+
+            #endregion
+
+            #region
+            //char gander;
+            //Console.WriteLine("cinsiyetınız nedir? (E/K)");
+            //gander = char.Parse(Console.ReadLine());
+
+            //Console.WriteLine("cınsıyetiniz: "  + gander);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+            Console.Read();
+
+
+        }
+    }
+}
